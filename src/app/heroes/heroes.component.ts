b43e43d1ff4be09c066201e13efe8277c25eacc3
wehrle-jl <wehrle5u@etu.univ-lorraine.fr>
2020-11-27 11:52:37 +0100

    tp4 affichage des héros

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES ;
   hero: Hero = {
-    id: 1,
+    id: 0,
     name: 'Windstorm'
   };
 
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'mouton' },
+  { id: 2, name: 'moutonman' },
+  { id: 3, name: 'battlesheep' },
+  { id: 4, name: 'sheepbattle' },
+  { id: 5, name: 'moutmout' },
+  { id: 6, name: 'tropfortenfait' },
+  { id: 7, name: 'unherosdeplus' },
+  { id: 8, name: 'sheepsheep' },
+  { id: 9, name: 'dieumoutonnien' },
+  { id: 10, name: 'mou-thon' }
+];
