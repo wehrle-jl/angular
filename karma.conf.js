24182cfe5a3ea581f07b467b654ef4c54a851590
wehrle-jl <wehrle5u@etu.univ-lorraine.fr>
2020-12-18 18:05:12 +0100

    fin

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-tour-of-heroes'),
+      dir: require('path').join(__dirname, './coverage/angular.io-example'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../message.service';
+
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
@@ -9,7 +10,7 @@ export class MessagesComponent implements OnInit {
 
   constructor(public messageService: MessageService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
